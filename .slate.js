a174a4fd6c1b985941b2be654d02db9a2d20590c
Josh Lebedinsky <joshlebed@gmail.com>
2023-03-09 09:53:16 -0500

    fix chrome find window bug and cleanup

--- .slate.js
+++ .slate.js
@@ -1,34 +1,33 @@
 const debug = true;
-const bigDebug = false;
 
-if (debug) slate.log("debug: reloaded");
+slate.log("reloaded slate");
 const pad = (object, width, direction = "left") =>
   direction == "left"
     ? (`${object}` + " ".repeat(width)).slice(0, width)
     : (" ".repeat(width) + `${object}`).slice(-width);
 
-const blacklist = ["Find in page*", "Bloomberg Anywhere"];
+// TODO: currently length 12 is hardcoded, make this smarter if we add more prefixes
+const blacklist_prefixes = new Set(["Find in page", "MenuBarCover"]);
 const debugWindow = (window, label) => {
   const rect = window.rect();
   const left = rect.x;
   const width = rect.width;
   const x = left + width / 2;
-  if (debug)
-    slate.log(
-      `debug: ` +
-        pad(label, 10) +
-        ` ` +
-        pad(left, 5) +
-        `, ` +
-        pad(left + width, 5) +
-        ` ` +
-        pad(window.pid(), 10) +
-        ` ` +
-        pad(window.app().name(), 20) +
-        ` ` +
-        pad(window.title(), 30) +
-        " "
-    );
+  slate.log(
+    `debug: ` +
+      pad(label, 10) +
+      ` ` +
+      pad(left, 5) +
+      `, ` +
+      pad(left + width, 5) +
+      ` ` +
+      pad(window.pid(), 10) +
+      ` ` +
+      pad(window.app().name(), 20) +
+      ` ` +
+      pad(window.title(), 30) +
+      " "
+  );
 };
 
 const hashString = (str) => {
@@ -96,10 +95,7 @@ const validWindow = (win) => {
   return (
     !win.isMinimizedOrHidden() &&
     title.length > 0 &&
-    !blacklist.reduce(
-      (result, regex) => result || title.match(regex) != null,
-      false
-    )
+    !blacklist_prefixes.has(title.substring(0, 12))
   );
 };
 
@@ -109,9 +105,8 @@ const getCenterX = (win) => {
 };
 
 const directionalFocus = (currentWin, direction) => {
-  if (bigDebug) slate.log("running");
   if (currentWin == null) {
-    if (debug) slate.log("debug: null currentWin");
+    slate.log("debug: null currentWin");
     target = null;
     slate.eachApp((app) => {
       app.eachWindow((win) => {
@@ -121,48 +116,20 @@ const directionalFocus = (currentWin, direction) => {
       });
     });
   } else {
-    if (debug) debugWindow(currentWin, "-> current");
+    debugWindow(currentWin, "-> current");
     var target = null;
     const windows = [];
     slate.eachApp((app) => {
       app.eachWindow((win) => {
-        if (validWindow(win)) {
+        debugWindow(win, "        found window: ");
+        if (validWindow(win) | sameWindow(win, currentWin)) {
           windows.push(win);
-
-          // const x = getCenterX(win);
-          // const xDiff = multiplier * (x - currentX);
-          // const hashDiff =
-          //   multiplier * (hashWindow(win) - hashWindow(currentWin));
-          // // debugWindow(win, "  (maybe)");
-          // // if (debug)
-          // // slate.log("debug:     diff " + xDiff + " tiebreaker " + hashDiff);
-
-          // slate.log(
-          //   `debug:      ` +
-          //     pad(win.app().name(), 20) +
-          //     " xDiff:" +
-          //     pad(xDiff, 6, "right") +
-          //     " hashDiff:" +
-          //     pad(hashDiff, 15, "right") +
-          //     ""
-          // );
-          // if (xDiff >= 0 && xDiff < minXDiff) {
-          //   if (xDiff > 0) {
-          //     minXDiff = xDiff;
-          //     minHashDiff = hashDiff;
-          //     target = win;
-          //   } else if (hashDiff > 0 && hashDiff < minHashDiff) {
-          //     minXDiff = xDiff;
-          //     minHashDiff = hashDiff;
-          //     target = win;
-          //   }
-          // }
         }
       });
     });
     windows.sort(windowSorter);
     windows.forEach((win) => {
-      if (debug) slate.log(`debug:      ` + pad(win.app().name(), 20) + "");
+      slate.log(`debug:      ` + pad(win.app().name(), 20) + "");
     });
     if (direction == "left") {
       windows.reverse();
@@ -179,20 +146,16 @@ const directionalFocus = (currentWin, direction) => {
   }
   if (target != null) {
     debugWindow(target, "-> target");
-    if (debug) slate.log("debug: ");
     target.focus();
-    if (bigDebug) slate.log("done!");
     return;
   }
-  if (bigDebug) slate.log("done, no action");
+  slate.log("didn't switch windows");
 };
 
 slate.bind("f16", function (win) {
   directionalFocus(win, "right");
-  // if (debug) slate.log("AAAAAAAAA");
 });
 
 slate.bind("f16:shift", function (win) {
   directionalFocus(win, "left");
-  // if (debug) slate.log("BBBBBBBBB");
 });
