da5e2e3ea1040c2c11d80e8c8e26d891991f3e6e
Josh Lebedinsky <joshlebed@gmail.com>
2024-10-25 12:15:28 -0400

    add some more raycast shortcuts

---
+++ raycast/scripts/open-bank-of-america.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.mode compact
+
+# @raycast.title BofA
+# @raycast.icon 🏦
+# @raycast.packageName open bank of america login page
+
+open "https://secure.bankofamerica.com/login/sign-in/signOnV2Screen.go"
---
+++ raycast/scripts/open-net-worth.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.mode compact
+
+# @raycast.title Net Worth
+# @raycast.icon 💰
+# @raycast.packageName open personal capital net worth page
+
+open "https://home.personalcapital.com/page/login/app#/net-worth"
