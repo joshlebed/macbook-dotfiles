601e6405825719970bfa671778902fc4bf1f6a56
Josh Lebedinsky <joshlebed@gmail.com>
2024-10-25 12:15:28 -0400

    add some more raycast shortcuts

---
+++ raycast/scripts/open-amex.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.mode compact
+
+# @raycast.title Amex
+# @raycast.icon 💳
+# @raycast.packageName open american express login page
+
+open "https://www.americanexpress.com/en-us/account/login"
---
+++ raycast/scripts/open-personal-capital.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.mode compact
+
+# @raycast.title Budget
+# @raycast.icon 💰
+# @raycast.packageName open personal capital budget page
+
+open "https://home.personalcapital.com/page/login/app#/all-transactions/CREDIT_CARD"
