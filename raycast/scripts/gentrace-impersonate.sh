4135a564730fb5fb58734f1c39a907ca0c1dcd00
Josh Lebedinsky <joshlebed@gmail.com>
2024-12-21 01:33:27 -0500

    karabiner updates, new scripts

---
+++ raycast/scripts/gentrace-impersonate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.mode compact
+
+# @raycast.title Impersonate
+# @raycast.icon 🤖
+# @raycast.packageName impersonate a user on gentrace
+
+code $HOME/code/gentrace-dev-shell-scripts/gentrace_aliases.sh
+open http://localhost:3000/impersonator
