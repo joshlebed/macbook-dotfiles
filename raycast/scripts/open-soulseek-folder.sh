30c27ed51b6f1c0389678532c4a732b2b46f9854
Josh Lebedinsky <joshlebed@gmail.com>
2025-02-24 20:56:13 -0500

    music stuff

---
+++ raycast/scripts/open-soulseek-folder.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# @raycast.schemaVersion 1
+# @raycast.mode compact
+
+# @raycast.title Soulseek folder
+# @raycast.icon 🎧
+# @raycast.packageName open soulseek folder
+
+open $HOME/Music/soulseek
+
